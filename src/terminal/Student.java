67d12c4f26a867f2010cefccc588d111c51c4591
admin <tnehdtlrl@naver.com>
2020-06-22 14:06:53 +0900

    0622

---
+++ src/terminal/Student.java
@@ -0,0 +1,47 @@
+package terminal;
+
+public class Student {
+	public enum Sex {
+		MALE, FEMALE;
+	}
+	public enum City {
+		Seoul, Busan;
+	}
+	private String name;
+	private int score;
+	private Sex sex;	// 올수있는 타입은 Male, Female
+	private City city;	// 올수있는 타입은 Seoul, Busan
+	
+	public Student(String name, int score, Sex sex) {
+		super();
+		this.name = name;
+		this.score = score;
+		this.sex = sex;
+	}
+
+	public Student(String name, int score, Sex sex, City city) {
+		super();
+		this.name = name;
+		this.score = score;
+		this.sex = sex;
+		this.city = city;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public Sex getSex() {
+		return sex;
+	}
+
+	public City getCity() {
+		return city;
+	}
+	
+	
+}
